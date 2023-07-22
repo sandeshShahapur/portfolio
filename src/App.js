bbc59a704ab9f42678db07a969a7c67b02315071
sandeshShahapur <sandeshshahapur@gmail.com>
2023-07-22 07:47:57 +0530

    testing 'exact

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/" exact element={<Layout />}>
+        <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="/About" element={<About />} />
           <Route path="/Contact" element={<Contact />} />
